ecd016edc2d13055cd5395f6b463f9e5d8d0f5f2
rlgaines <robertlg35@gmail.com>
2016-01-27 14:20:40 -0700

    new img file

--- src/js/main.js
+++ src/js/main.js
@@ -39,3 +39,67 @@ $("#allColors").on('click', function(){
 $("#allOccasions").on('click', function(){
   $(".anim").removeClass("removeOccasion");
 })
+
+
+
+//////////////////////Carousel//////////////////////
+
+
+$(document).on('ready', function() {
+
+var images = [
+ '1200x500?text=Image%201',
+ '1200x500?text=Image%202',
+ '1200x500?text=Image%203',
+ '1200x500?text=Image%204',
+ '1200x500?text=Image%205',
+];
+
+images.forEach(function(img, i) {
+  var imgURI = "http://placehold.it/" + img;
+  console.log(imgURI);
+  var imgName = "image-" + (i+1);
+  var imgDiv = "<div class= "+imgName+"></div>"
+  $(".carousel-images").append(imgDiv);
+  $("."+imgName).css("background-image", "url("+imgURI+")");
+})
+var i = 0;
+var current = $('.carousel-images > div');
+$('#forward').on('click', imageIterator);
+$('#back').on('click', imageBack);
+
+setInterval(imageIterator, 1000);
+
+function imageIterator() {
+  $('div').css('z-index', '0');
+  $(current[0]).css('z-index', '1');
+  if(images.length-1 === i) {
+    i = 0;
+    current = $('.carousel-images > div');
+  } else {
+    i++;
+    current = current.next();
+
+  }
+}
+
+function imageBack() {
+  $('div').css('z-index', '0');
+  $(current[0]).css('z-index', '1');
+  if(images.length-1 === i) {
+    i = images.length;
+    current = $('.carousel-images > div');
+  } else {
+    i--;
+    current = current.prev();
+
+  }
+}
+
+});
+
+
+
+
+
+
